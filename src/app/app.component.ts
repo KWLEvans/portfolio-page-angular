1156b06a95055ed016730944fba6153317f1462c
Keith Evans <kwlevans@gmail.com>
2017-04-27 16:49:46 -0700

    refactor add project.model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from './about/about.component';
 import { ProjectTileComponent } from './project-tile/project-tile.component';
+import { Project } from './project.model';
 // import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
@@ -12,8 +13,13 @@ export class AppComponent {
   about: boolean = false;
   contact: boolean = false;
   resume: boolean = false;
+  projects: Project[] = [
+    new Project('CODEpendency', 'The CODEpendency app allows users to create decks of flashcards for study as well as view all those that others have created.', './../assets/CODEpendency.png', 'https://codependency-45156.firebaseapp.com/'),
+    new Project('Life is Hell', 'Life is Hell is an early attempt at a game using JavaScript. I created custom functionality for object collision and generation.', './../assets/life-is-hell.png', 'https://kwlevans.github.io/LifeIsHell/')
+  ];
 
   toggleAbout() {
+    console.log(this.projects)
     if (this.about) {
       this.about = false;
     } else {
