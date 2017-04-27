9f08536871193f85f981e3ca0573543899077330
Keith Evans <kwlevans@gmail.com>
2017-04-27 12:07:24 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  about: boolean = false;
+  contact: boolean = false;
+
+  toggleAbout() {
+    if (this.about) {
+      this.about = false;
+    } else {
+      this.wipe();
+      this.about = true;
+    }
+  }
+
+  toggleContact() {
+    if (this.contact) {
+      this.contact = false;
+    } else {
+      this.wipe();
+      this.contact = true;
+    }
+  }
+
+  wipe() {
+    this.about = false;
+    this.contact = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
+import { masterFirebaseConfig } from './api-keys';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
